2d1fd4d7c22c7dded41193c7170a70f44bbdef95
mariosinnadamas <mariosaeztellez@gmail.com>
2025-10-14 10:21:49 +0200

    Ejercicio de Dani

---
+++ Tema2/Bucles/EjDani.py
@@ -0,0 +1,13 @@
+'''
+Haz una pirámide de asteriscos pidiendo el número de lineas al usuario.
+'''
+
+try:
+    n = int(input("Introduce el número de lineas de la pirámide: "))
+    for i in range (1, n+1):
+        espacios = " " * (n-i)
+        asteriscos = "*" * (2*i-1)
+        print(espacios + asteriscos)
+
+except ValueError:
+    print("ERROR: Introduce un número")
