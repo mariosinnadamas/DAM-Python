0cfaeb9ad68f6a6d519a2922fc0a4438bb782a61
mariosinnadamas <mariosaeztellez@gmail.com>
2025-10-19 22:50:29 +0200

    Ej5

---
+++ Tema2/Bucles2/Ej5.py
@@ -0,0 +1,24 @@
+def menu_restaurante():
+    # Menús de opciones y precios
+    platos = {
+        1: ("Pizza", 8.50),
+        2: ("Hamburguesa", 7.00),
+        3: ("Ensalada", 6.00)
+    }
+
+    bebidas = {
+        1: ("Agua", 1.50),
+        2: ("Refresco", 2.00),
+        3: ("Cerveza", 2.50)
+    }
+
+    complementos = {
+        1: ("Patatas", 2.50),
+        2: ("Ensalada pequeña", 2.00),
+        3: ("Pan de ajo", 3.00)
+    }
+
+    metodos_pago = {
+        1: "Efectivo",
+        2: "Tarjeta"
+    }
