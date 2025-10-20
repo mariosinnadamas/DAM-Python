a96584d2e8ef38c17369bc576c49bdb811db6b36
mariosinnadamas <mariosaeztellez@gmail.com>
2025-10-20 19:26:05 +0200

    Ej5

--- Tema2/Bloque2/Ej5.py
+++ Tema2/Bloque2/Ej5.py
@@ -6,4 +6,19 @@ Crea una calculadora que permita al usuario introducir dos números y una operac
 - Manejar operaciones no reconocidas.
 - Usar try-except con múltiples bloques para capturar distintos errores.
 '''
-
+try:
+    n1 = float(input("Introduce n1: "))
+    n2 = float(input("Introduce n2: "))
+    op = input("Introduce la operación que quieras realizar (+ o /): ")
+    if op == "+":
+        print(n1+n2)
+    elif op == "/":
+        try:
+            resultado = n1/n2
+            print(resultado)
+        except ZeroDivisionError:
+            print("ERROR: No se puede dividir entre 0")
+        else:
+            print("Operación no válida")
+except ValueError:
+    print("ERROR: Número introducido no válido")
