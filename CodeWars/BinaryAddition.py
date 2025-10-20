d32a2d1c1aa233c1b5be6ad3d6ad824bccc2a017
mariosinnadamas <mariosaeztellez@gmail.com>
2025-10-20 21:13:05 +0200

    Ej binario CodeWars

---
+++ CodeWars/BinaryAddition.py
@@ -0,0 +1,23 @@
+'''
+Escribes dos números y te devuelve la suma en binario
+'''
+from math import floor
+
+#Esto es lo que se me ocurrió a mi
+def add_binary(a,b):
+    suma = a + b
+    cociente = 1
+    nBinario = ""
+    while suma >0:
+        resto = suma % 2
+        cociente = suma // 2
+        suma = cociente
+        nBinario += str(resto)
+    return nBinario[::-1]
+
+#Esto es la respuesta más dada xd
+def add_binary2(a,b):
+    return bin(a+b)[2:]
+
+print(add_binary(5,5))
+print(add_binary2(5,5))
