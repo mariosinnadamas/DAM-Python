1ea9a5bd24a9927ff7e806caf211c8321aa8b2da
mariosinnadamas <mariosaeztellez@gmail.com>
2025-10-23 09:22:51 +0200

    CodeWars

---
+++ CodeWars/NarcissisticNumber.py
@@ -0,0 +1,22 @@
+'''
+Is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given
+base. In this Kata, we will restrict ourselves to decimal (base 10)
+For example, take 153 (3 digits), which is narcissistic:
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+and 1652 (4 digits), which isn't:
+    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+'''
+
+def narcissistic(value):
+    resultant = 0
+    cadena = str(value)
+    for digit in cadena:
+        d = int(digit)
+        resultant = d**len(cadena) + resultant
+
+    if resultant == value:
+        return True
+
+    return False
+
+print(narcissistic(153))
